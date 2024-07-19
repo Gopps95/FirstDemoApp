e4645a715524e84021bf700a0dd8e6ea82f34ac5
Gopi Shankar S <84285449+Gopps95@users.noreply.github.com>
2024-07-19 12:45:36 +0530

    intial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+using LogicLibrary;
+namespace FirstDemoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Hello, World!");
+           Console.Write("Hello.....");
+           Console.WriteLine();
+           double ans = Math.Sqrt(100);
+           Console.WriteLine(ans);
+           Products obj = new Products();
+           string s = obj.Greet();
+           Console.WriteLine(s);
+           Console.Read();
+
+            
+
+        }
+    }
+
+}
